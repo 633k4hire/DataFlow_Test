a3202e7187c6d61fd46824dd2bdb831f21bb7c21
All your base are belong to us <metzger.matthew@gmail.com>
2023-06-19 21:15:09 -0400

    Add project files.

---
+++ DataFlow_Test/DataFlowTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace DataFlow_Test
+{
+   public class PipeStage
+    {
+        public PipeStage(short input = 0)
+        {
+            Input = input;
+        }
+        public PipeStage(TransformBlock<PipeStage, PipeStage> block, short input=0)
+        {
+            Block = block;
+            Input = input;
+        }
+        public TransformBlock<PipeStage, PipeStage> Block;
+        public ActionBlock<PipeStage> EndBlock;
+        public short FinalAnswer; 
+        public short Input;
+        public void LinkTo(PipeStage stage)
+        {
+            Block.LinkTo(stage.Block, new DataflowLinkOptions() { PropagateCompletion = true });
+        }
+        public void LinkTail(PipeStage stage)
+        {
+            Block.LinkTo(stage.EndBlock, new DataflowLinkOptions() { PropagateCompletion = true });
+        }
+    }
+
+    public class ProcessingPipeline
+    {
+        // Define the blocks
+        private PipeStage Stage1;
+        private PipeStage Stage2;
+        private PipeStage Stage3;
+        private PipeStage EndPipe;
+        public short Answer;
+        private TaskCompletionSource<short> task1;
+        
+
+        public ProcessingPipeline()
+        {
+           
+        }
+
+        public void CreatePipeline()
+        {
+            var t = new ExecutionDataflowBlockOptions();
+            t.MaxDegreeOfParallelism = -1;
+            t.EnsureOrdered = true;
+            // Initialize the blocks
+
+
+            EndPipe = new PipeStage();
+            EndPipe.EndBlock = new ActionBlock<PipeStage>(endStage);
+
+            task1 = new TaskCompletionSource<short>();
+
+            Stage1 = new PipeStage(new TransformBlock<PipeStage, PipeStage>(stage1, t), 10);
+            Stage2 = new PipeStage(new TransformBlock<PipeStage, PipeStage>(stage2, t));
+            Stage3 = new PipeStage(new TransformBlock<PipeStage, PipeStage>(stage3, t));
+
+            Stage1.LinkTo(Stage2);
+            Stage2.LinkTo(Stage3);
+            Stage3.LinkTail(EndPipe);
+
+        }
+
+        // A method to post data to the first block
+        public void PostData(PipeStage data)
+        {
+            if (!Stage1.Block.Post(data))
+            {
+                throw new InvalidOperationException("Failed to post data to the pipeline");
+            }
+        }
+
+      
+        // A method to complete the pipeline and wait for all data to be processed
+        public async Task CompleteAndWaitAsync()
+        {
+            // Stage1.Block.Complete();
+           // await task1.Task;
+            await EndPipe.EndBlock.Completion;
+            var t = new ExecutionDataflowBlockOptions();
+            t.MaxDegreeOfParallelism = -1;
+            t.EnsureOrdered = true;
+            Stage1 = new PipeStage(new TransformBlock<PipeStage, PipeStage>(stage1, t), 10);
+            Stage2 = new PipeStage(new TransformBlock<PipeStage, PipeStage>(stage2, t));
+            Stage3 = new PipeStage(new TransformBlock<PipeStage, PipeStage>(stage3, t));
+            EndPipe = new PipeStage();
+            EndPipe.EndBlock = new ActionBlock<PipeStage>(endStage);
+
+            Stage1.LinkTo(Stage2);
+            Stage2.LinkTo(Stage3);
+            Stage3.LinkTail(EndPipe);
+            try
+            {
+                PostData(Stage1);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+
+        public void endStage(PipeStage input)
+        {           
+            input.Input = (short)(input.Input * 2);
+            Answer = input.Input;
+            //end the pipeline 
+        }
+
+        public PipeStage stage1(PipeStage input )
+        {
+            Stage1.Block.Complete();
+            input.Input = (short)(input.Input *2);
+            return input;
+        }
+        public PipeStage stage2(PipeStage input)
+        {
+            
+            input.Input = (short)(input.Input +10 );
+            return input;
+        }
+        public PipeStage stage3(PipeStage input)
+        {
+           // Stage3.Block.Complete();           
+            input.Input = (short)(input.Input - 1);
+          
+            return input;
+        }
+    }
+}
---
+++ DataFlow_Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using SensorFusion;
+namespace DataFlow_Test
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            // Create a new pipeline
+            //var pipeline = new ProcessingPipeline();
+
+            //// Post some numbers to the pipeline
+
+            //PipeStage ps = new PipeStage(10);
+
+            //pipeline.PostData(ps);
+
+
+            //// Tell the pipeline that there's no more data coming, and wait for all data to be processed
+            //await pipeline.CompleteAndWaitAsync();
+
+            var pipeline = new AudioProcessingPipelinePro();
+
+            // Feed the buffer with 10 seconds of audio data
+            await pipeline.FeedDataAsync();
+
+            //// Now you can continue to feed the pipeline as needed
+            //// var feedDataTask = pipeline.FeedDataAsync();
+
+            //// After some time, complete the pipeline and wait for all data to be processed
+            //await Task.Delay(TimeSpan.FromMinutes(1));
+
+            //// feedDataTask.Dispose();
+
+            //await pipeline.CompleteAndWaitAsync();
+
+            Console.WriteLine("All data has been processed");
+        }
+    }
+}
